6b16c90282b52374f4e5948c33706679f2921ef2
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 15:52:37 +0100

    MODIFY USER MODEL ASSOCIATIONS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+    has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+    has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+    has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 end
