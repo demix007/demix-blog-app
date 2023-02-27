766151bf26bce0e2b2de81c784e3c44aa0150f48
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 18:59:07 +0100

    ADD TEST CASES FOR USER SPEC

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
   
   validates :name, presence: true, length: { in: 3..25 }
-  validates :post_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :bio, presence: true
   validates :photo, presence: true
 
