b31d5e32cafcc66640797eb29e19ce110e93a42f
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 19:07:47 +0100

    FIX LINTER ERRORS

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
-  
+
   validates :name, presence: true, length: { in: 3..25 }
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :bio, presence: true
