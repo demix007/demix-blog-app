e6ccb7384c65297e845d9ceb4169f7e224a2c07f
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 13:46:29 +0100

    ADD users_id AS FORIGN KEY TO POSTS TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_112725) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_124531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_112725) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_112725) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "users_id"
 end
