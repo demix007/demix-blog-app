84f90e1db7c33ff26dae8261e2affcbaf9e33f25
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 16:59:20 +0100

    ADD VALIDATION TO CHECK FOR NUMERICALITY OF COMMENTS COUNTER IN POST MODEL

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   validates :title, presence: true, length: { in: 3..250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
 
