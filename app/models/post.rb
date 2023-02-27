878ab7cd69aa6efb1b8a57ec1436eecceb093ff9
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 16:57:27 +0100

    ADD VALIDATION TO CHECK FOR TITLE PRESENCE AND TITLE LENGTH IN POST MODEL

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  validates :title, presence: true, length: { in: 3..250 }
 
   after_save :update_posts_counter
 
