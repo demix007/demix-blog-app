9ab9f0f4d54d61fbd6c09f19be0e02680d36c60e
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-27 19:06:36 +0100

    ADD TEST CASES FOR LIKE SPEC

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Like.new(author_id: 10, post_id: 20) }
+  before { subject.save }
+
+  it 'expects author_id to be present in the Like' do
+    subject.author_id.nil?
+    expect(subject).to_not be_valid
+  end
+
+  it 'expects author_id to be an integer' do
+    subject.author_id = 'abc'
+    expect(subject).to_not be_valid
+  end
+
+  it 'expects author_id to be valid' do
+    expect(subject.author_id).to eql 10
+  end
+
+  it 'expects post_id to be present in the Like' do
+    subject.post_id.nil?
+    expect(subject).to_not be_valid
+  end
+
+  it 'expects post_id to be an integer' do
+    subject.post_id = 'abc'
+    expect(subject).to_not be_valid
+  end
+
+  it 'expects post_id to be valid' do
+    expect(subject.post_id).to eql 20
+  end
 end
