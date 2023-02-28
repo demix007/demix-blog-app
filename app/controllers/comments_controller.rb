e9934db6204a2e636d86205bda454bb2703c80a2
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-28 20:20:48 +0100

    ADD LOGIC TO CREATE NEW COMMENT INTO THE COMMENTS CONTROLLER create METHOD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,16 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post = @post
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author.id, id: @post.id), notice: 'Comment added successfully'
+    else
+      flash.now[:alert] = @comment.errors.full_messages.first if @comment.errors.any?
+      render :new, status: 400
+    end
   end
 
   def destroy
