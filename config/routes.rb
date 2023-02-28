307f10685aa4963d04549ae1d5e9c18c986e1012
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-28 20:18:07 +0100

    GENERATE COMMENTS CONTROLLER WITH new, create and destroy ACTIONS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
+  get 'comments/destroy'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
