3921cb32ff955f0606a3202ab6965fb9264c10d7
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-28 20:01:38 +0100

    ADD THE create ACTION INTO THE POSTS CONTROLLER

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,20 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
+    @comments = @post.comments
     @post = @user.posts.find(params[:id])
+  end
+
+    def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully'
+    else
+      flash.now[:alert] = @post.errors.full_messages.first if @post.errors.any?
+      render :new, status: 400
+    end
   end
 end
