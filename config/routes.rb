fa0c0959f48bdd468442f99e4810da7822044856
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-28 06:35:14 +0100

    GENERATE USERS CONTROLLER WITH INDEX AND SHOW ACTIONS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
