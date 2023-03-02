62160fa7335b032daab0cb095d1c3501ff3154ab
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-02 14:18:53 +0100

    ADD TEST CASE FOR user_path LINK IN THE USER INDEX PAGE

--- spec/views_integration_test/user_index_spec.rb
+++ spec/views_integration_test/user_index_spec.rb
@@ -23,5 +23,12 @@ RSpec.describe 'users Index Page', type: :feature do
       expect(page).to have_content(@user1.name)
       expect(page).to have_content(@user2.name)
     end
+
+    it 'has the correct link' do
+      visit users_path
+      expect(page).to have_link(@user1.name, href: user_path(@user1))
+      expect(page).to have_link(@user2.name, href: user_path(@user2))
+    end
+
   end
 end
