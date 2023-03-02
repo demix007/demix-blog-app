eafa647022ae81b802ab129050545636d606cddb
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-02 14:13:58 +0100

    CREATE MOCK USER2 FOR user_index_spec

--- spec/views_integration_test/user_index_spec.rb
+++ spec/views_integration_test/user_index_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe 'users Index Page', type: :feature do
         bio: 'Student at Microverse',
         posts_counter: 8
       )
+
+      @user2 = User.create!(
+        name: 'Abdul Saboor', 
+        photo: 'https://unsplash.com/photos/WNoLnJo7tS8', 
+        bio: 'Full Stack web Developer',
+        posts_counter: 9
+      )
+    end
   end
 end
