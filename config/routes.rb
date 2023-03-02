1d889ba89861c58495408db8e2edb1406bda7571
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-02 14:06:44 +0100

    MODIFY ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  # root "articles#index"
   root 'users#index', as: 'home'
+
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:create]
       resources :comments, only: [:new, :create]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
